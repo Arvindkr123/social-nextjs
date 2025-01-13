9de803e076235ecf56fb960f2cd2ed9ee84fc628
= <=>
2025-01-13 16:44:36 +0530

    added setup with prisma and sync user

---
+++ src/actions/user.actions.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import prisma from "@/lib/primsa";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    console.log("user", user, userId)
+
+    if (!userId || !user) return;
+
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
